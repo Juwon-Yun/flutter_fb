baef7e798e4c5d92a7ab1defabb5633bf739a469
Juwon-Yun <eooll2@daum.net>
2022-07-28 23:40:19 +0900

    modify: detail screen

--- lib/view/detail_screen.dart
+++ lib/view/detail_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_fb/model/movie_model.dart';
 
@@ -21,6 +23,7 @@ class _DetailScreenState extends State<DetailScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        color: Colors.black45,
         child: SafeArea(
           child: ListView(
             children: [
@@ -35,10 +38,103 @@ class _DetailScreenState extends State<DetailScreen> {
                         fit: BoxFit.cover,
                       ),
                     ),
+                    child: ClipRect(
+                      child: BackdropFilter(
+                        filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                        child: Container(
+                          alignment: Alignment.center,
+                          color: Colors.black.withOpacity(0.1),
+                          child: Container(
+                            child: Column(
+                              children: [
+                                Container(
+                                  padding: EdgeInsets.fromLTRB(0, 45, 0, 10),
+                                  child: Image.asset(
+                                    'assets/images/${widget.movie.poster}',
+                                    height: 300,
+                                  ),
+                                ),
+                                Container(
+                                  padding: EdgeInsets.all(7),
+                                  child: Text(
+                                    '99% 일치 2022 15+ 시즌 1개',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                        fontSize: 13, color: Colors.white),
+                                  ),
+                                ),
+                                Container(
+                                  padding: EdgeInsets.all(7),
+                                  child: Text(
+                                    widget.movie.title,
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  padding: EdgeInsets.all(3),
+                                  child: FlatButton(
+                                    onPressed: () {},
+                                    color: Colors.red,
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Icon(
+                                          Icons.play_arrow,
+                                          color: Colors.white,
+                                        ),
+                                        Text(
+                                          '재생',
+                                          style: TextStyle(
+                                            fontSize: 16,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  padding: EdgeInsets.all(5),
+                                  child: Text(
+                                    widget.movie.toString(),
+                                    style: TextStyle(
+                                      fontSize: 12,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  padding: EdgeInsets.all(5),
+                                  alignment: Alignment.centerLeft,
+                                  child: Text(
+                                    '출연: 현빈, 손예진, 서지혜\n제작자: 이정효, 박지은',
+                                    style: TextStyle(
+                                      fontSize: 12,
+                                      color: Colors.white60,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
                   ),
+                  Positioned(
+                    child: AppBar(
+                      backgroundColor: Colors.transparent,
+                      elevation: 0,
+                    ),
+                  )
                 ],
               ),
-              makeMenuButton(),
+              makeMenuButton(like),
             ],
           ),
         ),
@@ -47,4 +143,29 @@ class _DetailScreenState extends State<DetailScreen> {
   }
 }
 
-Widget makeMenuButton() => Container();
+Widget makeMenuButton(bool like) => Container(
+      color: Colors.black26,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+            child: InkWell(
+              onTap: () {},
+              child: Column(
+                children: [
+                  like ? Icon(Icons.check) : Icon(Icons.add),
+                  Padding(
+                    padding: EdgeInsets.all(5),
+                  ),
+                  Text(
+                    '내가 찜한 콘텐츠',
+                    style: TextStyle(fontSize: 11, color: Colors.white60),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
