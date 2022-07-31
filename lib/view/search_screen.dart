cd460830a66c0c881eb73d38b513330b7df5327f
Juwon-Yun <eooll2@daum.net>
2022-07-31 11:51:13 +0900

    feature: search screen

--- lib/view/search_screen.dart
+++ lib/view/search_screen.dart
@@ -26,68 +26,80 @@ class _SearchScreenState extends State<SearchScreen> {
       padding: EdgeInsets.only(top: 40),
       child: Column(
         children: [
-          Container(
-            color: Colors.black,
-            padding: EdgeInsets.fromLTRB(5, 10, 5, 10),
-            child: Row(
-              children: [
-                Expanded(
-                  flex: 6,
-                  child: TextField(
-                    focusNode: focusNode,
-                    style: TextStyle(fontSize: 15),
-                    autofocus: true,
-                    controller: _filter,
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white12,
-                      prefixIcon: Icon(
-                        Icons.search,
-                        color: Colors.white60,
-                        size: 20,
-                      ),
-                      suffixIcon: focusNode.hasFocus
-                          ? IconButton(
-                              onPressed: () {
-                                setState(() {
-                                  _filter.clear();
-                                  _searchText = '';
-                                });
-                              },
-                              icon: Icon(
-                                Icons.cancel,
-                                size: 20,
-                                color: Colors.white60,
-                              ))
-                          : Container(),
-                      hintText: '검색',
-                      labelStyle: TextStyle(color: Colors.white),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.transparent),
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(10),
+          GestureDetector(
+            onTap: () {
+              hideKeyboard(context);
+            },
+            child: Container(
+              color: Colors.black,
+              padding: EdgeInsets.fromLTRB(5, 10, 5, 10),
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 6,
+                    child: TextField(
+                      focusNode: focusNode,
+                      style: TextStyle(fontSize: 15),
+                      autofocus: true,
+                      controller: _filter,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white12,
+                        prefixIcon: Icon(
+                          Icons.search,
+                          color: Colors.white60,
+                          size: 20,
                         ),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.transparent),
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(10),
+                        suffixIcon: focusNode.hasFocus
+                            ? IconButton(
+                                onPressed: () {
+                                  setState(() {
+                                    _filter.clear();
+                                    _searchText = '';
+                                  });
+                                },
+                                icon: Icon(
+                                  Icons.cancel,
+                                  size: 20,
+                                  color: Colors.white60,
+                                ))
+                            : Container(),
+                        hintText: '검색',
+                        labelStyle: TextStyle(color: Colors.white),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.transparent),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10),
+                          ),
                         ),
-                      ),
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.transparent),
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(10),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.transparent),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10),
+                          ),
+                        ),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.transparent),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10),
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           )
         ],
       ),
     );
+  }
+}
+
+void hideKeyboard(BuildContext context) {
+  FocusScopeNode currentFocus = FocusScope.of(context);
+  if (!currentFocus.hasPrimaryFocus && currentFocus.focusedChild != null) {
+    FocusManager.instance.primaryFocus?.unfocus();
   }
 }
