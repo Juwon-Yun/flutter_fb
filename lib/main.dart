915e3f7e5be96fa3f80a0809d0f2d46292aadc20
Juwon-Yun <eooll2@daum.net>
2022-09-03 23:44:01 +0900

    modify: fcm config

--- lib/main.dart
+++ lib/main.dart
@@ -7,15 +7,54 @@ import 'package:flutter_fb/view/home_screen.dart';
 import 'package:flutter_fb/view/like_screen.dart';
 import 'package:flutter_fb/view/more_screen.dart';
 import 'package:flutter_fb/view/search_screen.dart';
+import 'package:logger/logger.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  final fcmToken = await FirebaseMessaging.instance.getToken();
+  var logger = Logger();
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+  logger.d('fcmToken ::: $fcmToken');
+
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+
+  print('User granted permission: ${settings.authorizationStatus}');
+
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.data}');
+
+    if (message.notification != null) {
+      print('Message also contained a notification: ${message.notification}');
+    }
+  });
+
+  Future<void> _firebaseMessagingBackgroundHandler(
+      RemoteMessage message) async {
+    // If you're going to use other Firebase services in the background, such as Firestore,
+    // make sure you call `initializeApp` before using other Firebase services.
+    await Firebase.initializeApp();
+
+    print("Handling a background message: ${message.messageId}");
+  }
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
   runApp(const FirebaseExample());
 }
 
